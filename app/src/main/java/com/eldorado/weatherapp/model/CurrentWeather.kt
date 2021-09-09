411ef1d991e2495f6bf8a4a8c844b50f2bdc511d
Leonardo Santana <leonardossnt@gmail.com>
2021-09-08 21:46:46 -0400

    Add Data class for Weather View

--- app/src/main/java/com/eldorado/weatherapp/model/CurrentWeather.kt
+++ app/src/main/java/com/eldorado/weatherapp/model/CurrentWeather.kt
@@ -12,6 +12,15 @@ data class CurrentWeather(
     val sys: Sys,
 //    val timezone: Int,
 //    val visibility: Int,
-    val weather: List<Weather>
-//    val wind: Wind
+    val weather: List<Weather>,
+    val wind: Wind
+)
+fun CurrentWeather.toWeatherView() = WeatherView(
+    name,
+    String.format("%.1f", main.temp),
+    weather[0].description,
+    "${String.format("%.1f", main.feels_like)}\u2103",
+    "${String.format("%.1f", wind.speed)}m/s",
+    "${main.humidity}%",
+    "https://openweathermap.org/img/wn/${weather[0].icon}@2x.png"
 )
---
+++ app/src/main/java/com/eldorado/weatherapp/model/WeatherView.kt
@@ -0,0 +1,11 @@
+package com.eldorado.weatherapp.model
+
+class WeatherView(
+    val locationName : String,
+    val temperature : String,
+    val description : String,
+    val feelsLike : String,
+    val wind : String,
+    val humidity : String,
+    val iconUrl : String
+)
